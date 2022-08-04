b6025e8c7e5056d5bfb7089b2e973c86a7179ea6
djlee <djlee@kslab.co.kr>
2022-08-04 15:35:40 +0900

    home/end 키 매핑 단순하게 개선

--- .hammerspoon/module/restore_home_and_end_key.lua
+++ .hammerspoon/module/restore_home_and_end_key.lua
@@ -3,12 +3,8 @@
 
 -- home/end
 hs.hotkey.bind({''}, 'home', nil, function() hs.eventtap.keyStroke({'command'}, 'left')end)
-hs.hotkey.bind({"ctrl"}, "home", nil, function() hs.eventtap.keyStroke({"cmd"}, "up")end)
 hs.hotkey.bind({""}, "end", nil, function() hs.eventtap.keyStroke({"cmd"}, "right") end)
-hs.hotkey.bind({"ctrl"}, "end", nil, function() hs.eventtap.keyStroke({"cmd"}, "down") end)
 
 -- home/end + shift
 hs.hotkey.bind('shift','home', nil, function() hs.eventtap.keyStroke({'shift','cmd'},'left')end) 
 hs.hotkey.bind('shift','end', nil, function() hs.eventtap.keyStroke({'shift','cmd'},'right')end)
-hs.hotkey.bind({'shift','ctrl'},'home', nil, function() hs.eventtap.keyStroke({'shift','cmd'},'up')end)
-hs.hotkey.bind({'shift','ctrl'},'end', nil, function() hs.eventtap.keyStroke({'shift','cmd'},'down')end)
